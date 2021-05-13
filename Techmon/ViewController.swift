33b7bfb7c0a488eac2b81b718704cd1a97ac9514
Yuma Ikeda <yumaxikeda@gmail.com>
2021-05-13 16:25:13 +0900

    Initial Commit

---
+++ Techmon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techmon
+//
+//  Created by Yuma Ikeda on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechmonTests/TechmonTests.swift
@@ -0,0 +1,33 @@
+//
+//  TechmonTests.swift
+//  TechmonTests
+//
+//  Created by Yuma Ikeda on 2021/05/13.
+//
+
+import XCTest
+@testable import Techmon
+
+class TechmonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
